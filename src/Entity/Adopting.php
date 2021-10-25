aca2ccf217865ad3f5d1001d46c694a9cbca241d
Gagz71 <sangodoun@protonmail.com>
2021-10-25 11:35:54 +0200

    SCOUB-38 création entité Advertiser extends user

--- src/Entity/Adopting.php
+++ src/Entity/Adopting.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\AdoptingRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Entity;
 
 /**
  * @ORM\Entity(repositoryClass=AdoptingRepository::class)
---
+++ src/Entity/Advertiser.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdvertiserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Entity;
+
+/**
+ * @ORM\Entity(repositoryClass=AdvertiserRepository::class)
+ * @Entity
+ */
+class Advertiser extends User
+{
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $organizationName;
+
+
+
+    public function getOrganizationName(): ?string
+    {
+        return $this->organizationName;
+    }
+
+    public function setOrganizationName(string $organizationName): self
+    {
+        $this->organizationName = $organizationName;
+
+        return $this;
+    }
+}
