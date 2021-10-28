c45d05e84d0fda8f30ffe5edd836c2c766d38de9
Bazev <evan.bazin@gmail.com>
2021-10-28 11:45:01 +0200

    page accueil 5 dernieres annonces

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use App\Repository\AdvertRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class     HomeController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager, AdvertRepository $advertRepository)
+    {
+        $this->entityManager = $entityManager;
+        $this->advertRepository = $advertRepository;
+    }
+
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(AdvertRepository $advertRepository, EntityManagerInterface $entityManager): Response
     {
+
+        // appel repo function 5 last adverts
+        //need entity manager
+        $adverts = $this->advertRepository->callFiveAdverts();
+
         return $this->render('home/index.html.twig', [
+            'advertsHome' => $adverts,
             'controller_name' => 'HomeController',
         ]);
     }
--- src/Entity/Dog.php
+++ src/Entity/Dog.php
@@ -12,213 +12,213 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Dog
 {
-	/**
-	 * @ORM\Id
-	 * @ORM\GeneratedValue
-	 * @ORM\Column(type="integer")
-	 */
-	private $id;
-	
-	/**
-	 * @ORM\Column(type="string", length=255)
-	 */
-	private $name;
-	
-	/**
-	 * @ORM\Column(type="string", length=255, nullable=true)
-	 */
-	private $antecedents;
-	
-	/**
-	 * @ORM\Column(type="boolean")
-	 */
-	private $lof;
-	
-	/**
-	 * @ORM\Column(type="string", length=255, nullable=true)
-	 */
-	private $fullDescription;
-	
-	/**
-	 * @ORM\Column(type="boolean", nullable=true)
-	 */
-	private $sociable;
-	
-	/**
-	 * @ORM\Column(type="boolean")
-	 */
-	private $isAdopted;
-	
-	/**
-	 * @ORM\ManyToMany (targetEntity=AdoptingRequest::class, inversedBy="dogs")
-	 */
-	private $adoptingRequests;
-	
-	/**
-	 * @ORM\ManyToOne(targetEntity=Advert::class, inversedBy="dogs")
-	 */
-	private $advert;
-	
-	/**
-	 * @ORM\ManyToMany(targetEntity=Race::class, mappedBy="dog")
-	 */
-	private $races;
-	
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $antecedents;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $lof;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $fullDescription;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $sociable;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isAdopted;
+
+    /**
+     * @ORM\ManyToMany (targetEntity=AdoptingRequest::class, inversedBy="dogs")
+     */
+    private $adoptingRequests;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Advert::class, inversedBy="dogs")
+     */
+    private $advert;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Race::class, mappedBy="dog")
+     */
+    private $races;
+
     /**
      * @ORM\OneToMany(targetEntity=UrlPicture::class, mappedBy="dogs")
      */
     private $urlPictures;
-	
-	public function __construct()
-               	{
-               		$this->races = new ArrayCollection();
-                 $this->urlPictures = new ArrayCollection();
-               	}
-	
-	public function getId(): ?int
-               	{
-               		return $this->id;
-               	}
-	
-	public function getName(): ?string
-               	{
-               		return $this->name;
-               	}
-	
-	public function setName(string $name): self
-               	{
-               		$this->name = $name;
-               		
-               		return $this;
-               	}
-	
-	
-	public function getAntecedents(): ?string
-               	{
-               		return $this->antecedents;
-               	}
-	
-	public function setAntecedents(string $antecedents): self
-               	{
-               		$this->antecedents = $antecedents;
-               		
-               		return $this;
-               	}
-	
-	public function getLof(): ?bool
-               	{
-               		return $this->lof;
-               	}
-	
-	public function setLof(bool $lof): self
-               	{
-               		$this->lof = $lof;
-               		
-               		return $this;
-               	}
-	
-	public function getFullDescription(): ?string
-               	{
-               		return $this->fullDescription;
-               	}
-	
-	public function setFullDescription(string $fullDescription): self
-               	{
-               		$this->fullDescription = $fullDescription;
-               		
-               		return $this;
-               	}
-	
-	public function getSociable(): ?bool
-               	{
-               		return $this->sociable;
-               	}
-	
-	public function setSociable(bool $sociable): self
-               	{
-               		$this->sociable = $sociable;
-               		
-               		return $this;
-               	}
-	
-	public function getIsAdopted(): ?bool
-               	{
-               		return $this->isAdopted;
-               	}
-	
-	public function setIsAdopted(bool $isAdopted): self
-               	{
-               		$this->isAdopted = $isAdopted;
-               		
-               		return $this;
-               	}
-	
-	/**
-	 * @return Collection|AdoptingRequest[]
-	 */
-	public function getAdoptingRequests(): Collection
-               	{
-               		return $this->adoptingRequests;
-               	}
-	
-	public function addAdoptingRequest(AdoptingRequest $adoptingRequest): self
-               	{
-               		if (!$this->adoptingRequests->contains($adoptingRequest)) {
-               			$this->adoptingRequests[] = $adoptingRequest;
-               			$adoptingRequest->addDog($this);
-               		}
-               		
-               		return $this;
-               	}
-	
-	public function removeAdoptingRequest(AdoptingRequest $adoptingRequest): self
-               	{
-               		if ($this->adoptingRequests->removeElement($adoptingRequest)) {
-               			$adoptingRequest->removeDog($this);
-               		}
-               		
-               		return $this;
-               	}
-	
-	public function getAdvert(): ?Advert
-               	{
-               		return $this->advert;
-               	}
-	
-	public function setAdvert(?Advert $advert): self
-               	{
-               		$this->advert = $advert;
-               		
-               		return $this;
-               	}
-	
-	/**
-	 * @return Collection|Race[]
-	 */
-	public function getRaces(): Collection
-               	{
-               		return $this->races;
-               	}
-	
-	public function addRace(Race $race): self
-               	{
-               		if (!$this->races->contains($race)) {
-               			$this->races[] = $race;
-               			$race->addDog($this);
-               		}
-               		
-               		return $this;
-               	}
-	
-	public function removeRace(Race $race): self
-               	{
-               		if ($this->races->removeElement($race)) {
-               			$race->removeDog($this);
-               		}
-               		
-               		return $this;
-               	}
-				
+
+    public function __construct()
+    {
+        $this->races = new ArrayCollection();
+        $this->urlPictures = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+
+    public function getAntecedents(): ?string
+    {
+        return $this->antecedents;
+    }
+
+    public function setAntecedents(string $antecedents): self
+    {
+        $this->antecedents = $antecedents;
+
+        return $this;
+    }
+
+    public function getLof(): ?bool
+    {
+        return $this->lof;
+    }
+
+    public function setLof(bool $lof): self
+    {
+        $this->lof = $lof;
+
+        return $this;
+    }
+
+    public function getFullDescription(): ?string
+    {
+        return $this->fullDescription;
+    }
+
+    public function setFullDescription(string $fullDescription): self
+    {
+        $this->fullDescription = $fullDescription;
+
+        return $this;
+    }
+
+    public function getSociable(): ?bool
+    {
+        return $this->sociable;
+    }
+
+    public function setSociable(bool $sociable): self
+    {
+        $this->sociable = $sociable;
+
+        return $this;
+    }
+
+    public function getIsAdopted(): ?bool
+    {
+        return $this->isAdopted;
+    }
+
+    public function setIsAdopted(bool $isAdopted): self
+    {
+        $this->isAdopted = $isAdopted;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|AdoptingRequest[]
+     */
+    public function getAdoptingRequests(): Collection
+    {
+        return $this->adoptingRequests;
+    }
+
+    public function addAdoptingRequest(AdoptingRequest $adoptingRequest): self
+    {
+        if (!$this->adoptingRequests->contains($adoptingRequest)) {
+            $this->adoptingRequests[] = $adoptingRequest;
+            $adoptingRequest->addDog($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAdoptingRequest(AdoptingRequest $adoptingRequest): self
+    {
+        if ($this->adoptingRequests->removeElement($adoptingRequest)) {
+            $adoptingRequest->removeDog($this);
+        }
+
+        return $this;
+    }
+
+    public function getAdvert(): ?Advert
+    {
+        return $this->advert;
+    }
+
+    public function setAdvert(?Advert $advert): self
+    {
+        $this->advert = $advert;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Race[]
+     */
+    public function getRaces(): Collection
+    {
+        return $this->races;
+    }
+
+    public function addRace(Race $race): self
+    {
+        if (!$this->races->contains($race)) {
+            $this->races[] = $race;
+            $race->addDog($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRace(Race $race): self
+    {
+        if ($this->races->removeElement($race)) {
+            $race->removeDog($this);
+        }
+
+        return $this;
+    }
+
     /**
      * @return Collection|UrlPicture[]
      */
