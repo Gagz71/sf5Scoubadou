ef6c13f265f50ba25f6f74cdafbebcd8883d9574
stephane <you@example.com>
2021-10-29 13:45:37 +0200

    still workin on advertiser sheet

--- src/Controller/AdvertiserController.php
+++ src/Controller/AdvertiserController.php
@@ -2,7 +2,34 @@
 
 namespace App\Controller;
 
-class AdvertiserController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdvertiserController extends AbstractController
 {
 
+
+    /**
+     * @Route("/annonces/{id}", name = "advertiser_view")
+     */
+    public function advertiserView($id) {
+
+        //recupération de l'annonceur correspondant à l'id $id
+        $em = $this->getDoctrine()->getManager();
+        $advertiser = $em->getRepository('Advertiser')->find($id);
+
+        if( null === $advertiser) {
+            throw new NotFoundHttpException("La commande d'id ".$id." n'existe pas.");
+        }
+        $listAdverts = $em->getRepository('Advert')->findBy(array('advertiser' => $advertiser));
+        $listAdoptingRqs = $em->getRepository('AdoptingRequest')->findBy(array('advertiser' => $advertiser));
+
+        return $this->render('advertiser/view.html.twig', [
+            'advertiser' => $advertiser,
+            'listAdverts' => $listAdverts,
+            'listAdoptingRqs' => $listAdoptingRqs
+        ]);
+
+    }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -39,26 +39,4 @@ class     HomeController extends AbstractController
         ]);
     }
 
-    public function AdvertiserViewAction($id) {
-
-        //recupération de l'annonceur correspondant à l'id $id
-        $em = $this->getDoctrine()->getManager();
-        $advertiser = $em->getRepository('Advertiser')->find($id);
-
-        if( null === $advertiser) {
-            throw new NotFoundHttpException("La commande d'id ".$id." n'existe pas.");
-        }
-        $listAdverts = $em->getRepository('Advert')->findBy(array('advertiser' => $advertiser));
-        $listAdoptingRqs = $em->getRepository('AdoptingRequest')->findBy(array('advertiser' => $advertiser));
-
-        return $this->render('advertiser/view.html.twig', [
-            'advertiser' => $advertiser,
-            'listAdverts' => $listAdverts,
-            'listAdoptingRqs' => $listAdoptingRqs
-        ]);
-
-    }
-
-
-
 }
