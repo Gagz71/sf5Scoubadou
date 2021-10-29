aa52602fedd3a653eb9cfcf853d6658b994efe03
stephane <you@example.com>
2021-10-29 10:58:57 +0200

    homeController modified

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class     HomeController extends AbstractController
@@ -37,4 +38,27 @@ class     HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    public function AdvertiserViewAction($id) {
+
+        //recupération de l'annonceur correspondant à l'id $id
+        $em = $this->getDoctrine()->getManager();
+        $advertiser = $em->getRepository('Advertiser')->find($id);
+
+        if( null === $advertiser) {
+            throw new NotFoundHttpException("La commande d'id ".$id." n'existe pas.");
+        }
+        $listAdverts = $em->getRepository('Advert')->findBy(array('advertiser' => $advertiser));
+        $listAdoptingRqs = $em->getRepository('AdoptingRequest')->findBy(array('advertiser' => $advertiser));
+
+        return $this->render('advertiser/view.html.twig', [
+            'advertiser' => $advertiser,
+            'listAdverts' => $listAdverts,
+            'listAdoptingRqs' => $listAdoptingRqs
+        ]);
+
+    }
+
+
+
 }
