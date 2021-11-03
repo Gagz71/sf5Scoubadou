92a4cba502907500ae62fec05fe6003f0e23f0cd
bazev <evan.bazin@gmail.com>
2021-11-03 10:49:07 +0100

    améliorations Navar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Advert;
 use App\Repository\AdvertiserRepository;
 use App\Repository\AdvertRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\OrderBy;
+use Doctrine\ORM\Query\AST\OrderByItem;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -27,7 +30,7 @@ class     HomeController extends AbstractController
     public function index(AdvertRepository $advertRepository, EntityManagerInterface $entityManager): Response
     {
         $adverts = $this->advertRepository->callFiveAdverts();
-        $advertisers = $this->advertiserRepository->findAll();
+        $advertisers = $this->advertiserRepository->getAdvertsByDate();
 
         return $this->render('home/index.html.twig', [
             'advertsHome' => $adverts,
