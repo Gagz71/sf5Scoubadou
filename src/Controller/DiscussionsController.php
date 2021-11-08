e3ef2a50445c990dd375bb7aa6fc1672eddb0aee
stephane <you@example.com>
2021-11-08 14:22:48 +0100

    add discussion step 1

--- src/Controller/DiscussionsController.php
+++ src/Controller/DiscussionsController.php
@@ -5,22 +5,69 @@ namespace App\Controller;
 use App\Entity\Adopting;
 use App\Entity\AdoptingRequest;
 use App\Entity\Advert;
+use App\Entity\Discussion;
 use App\Entity\User;
+use App\Form\DiscussionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DiscussionsController extends AbstractController
 {
+
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/discussions/{id}", name="discussions")
      */
     public function showDiscussions(AdoptingRequest $adoptingRequest): Response
     {
-	    
+
         return $this->render('discussions/index.html.twig', [
 
             'adoptingRequest' => $adoptingRequest
+
+        ]);
+    }
+
+
+    /**
+     * @Route("/discussions/{id}", name="discussions")
+     */
+    public function addMessage(Request $request ,AdoptingRequest $adoptingRequest ,Advert $advert)
+        : Response
+
+    {
+
+        $discussion = new Discussion();
+        $discussion->setCreationDate(new \DateTime());
+        $adoptingRequest->addDiscussion($discussion);
+
+        $form = $this->createForm(DiscussionType::class, $discussion, [
+
+            ]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($discussion);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Nouveau message envoyé');
+            return $this->redirectToRoute('discussions', []);
+
+        }
+
+        $listDiscussions = $adoptingRequest->getDiscussions();
+
+        return $this->render('adopting_request/view.html.twig', [
+            'form' =>$form->createView(),
+            'listDiscussions' => $listDiscussions
 
         ]);
     }
